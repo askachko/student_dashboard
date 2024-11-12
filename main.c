6bec0c314b658fc5abd1f5fc3b2eeb602bf8b9dc
askachko <alexandermskachko@gmail.com>
2024-11-12 10:09:34 -0500

    able to read in text file, accepting user input

--- main.c
+++ main.c
@@ -1,12 +1,41 @@
 #include <stdio.h>
 
-char username[50] = "Alex";
+struct User {
+  char username[50];
+  char password[50];
+};
+
+char username[50];
+char password[50];
+
+void find_user_by_username(char username[50]);
 
 int main() {
+
   printf("Insert your username:");
   scanf("%49s", username);
 
-  printf("Username entered: %s\n", username);
+  printf("Insert your password:");
+  scanf("%49s", password);
+
+  FILE *fp;
+  char filename[] = "database.txt";
+  char line[256];
+
+  fp = fopen(filename, "r");
+
+  if (fp == NULL) {
+    perror("Error opening file");
+    return 1;
+  }
+
+  while (fgets(line, sizeof(line), fp) != NULL) {
+      printf("%s", line);
+  }
+
+  fclose(fp);
+
   return 0;
 }
 
+void find_user_by_username(char username[50]) {}
