756edb9f0db25f6f2eba524e32fc342015892d65
askachko <alexandermskachko@gmail.com>
2024-11-13 00:38:33 -0500

    added the add new user function, getting bus error, might be related to my pointers

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #include <unistd.h>
 
 struct User {
-  char username[50];
-  char password[50];
+  char *username;
+  char *password;
 };
 
 // for user input
@@ -16,6 +16,8 @@ char password[50];
 #define NUM_OPTIONS 4
 const char *pMenu_options[NUM_OPTIONS] = {"Grades    ", "Classes   ",
                                           "Upcoming  ", "GPA       "};
+
+#define FILE_NAME "database.txt"
 
 int cursor_position = 1;
 
@@ -32,6 +34,42 @@ struct User find_user_by_username(char username[50]);
 bool verify_user(struct User user, char entered_password[50]);
 void header(int col, char text[50]);
 void center_text(int width, const char *text);
+
+void create_user(struct User new_user) {
+
+  FILE *fptr = fopen(FILE_NAME, "a"); // Open the file for appending
+
+  if (fptr == NULL) {
+    perror("Error opening file");
+    return;
+  }
+
+  // Write the new user's details to the file
+  fprintf(fptr, "\"%s\", \"%s\"\n", new_user.username, new_user.password);
+  fclose(fptr);
+};
+
+void print_register(int width) {
+
+  char username[50];
+  char password[50];
+
+  printf("\033[2J\033[1;1H");
+  header(width, "Register");
+
+  while (1) {
+    printf("\n\n   Enter a username: ");
+    scanf("%s", username);
+
+    printf("\n\n   Enter a password: ");
+    scanf("%s", password);
+
+    struct User new_user = {username, password};
+
+    create_user(new_user);
+    break;
+  };
+};
 
 void print_menu(int width) {
 
@@ -51,9 +89,20 @@ int main(int argc, char **argv) {
   struct winsize w;
   ioctl(STDOUT_FILENO, TIOCGWINSZ, &w);
 
+  bool registered = true;
+
   header(w.ws_col, "Login");
   printf("\n\n   Insert your username: ");
+  printf("\n\n\n   Or enter (1) to register\n");
+
   scanf("%49s", username);
+
+  if (strcmp(username, "1") == 0) {
+    registered = false;
+  }
+
+  if (!registered)
+    print_register(w.ws_col);
 
   printf("\n\n   Insert your password: ");
   scanf("%49s", password);
@@ -79,9 +128,7 @@ int main(int argc, char **argv) {
   print_menu(w.ws_col);
 
   char k;
-  while (1){
-
-
+  while (1) {
   }
 
   return 0;
@@ -92,7 +139,7 @@ struct User find_user_by_username(char username[50]) {
   struct User user = {"", ""};
 
   FILE *fp;
-  char filename[] = "database.txt";
+  char filename[] = FILE_NAME;
   char line[256];
 
   fp = fopen(filename, "r");
